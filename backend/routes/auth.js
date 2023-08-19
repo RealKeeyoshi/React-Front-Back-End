defa522bf6d8b43d08ea32cc538f53db35cbaf64
Keeyoshi Pyakurel <keeyoshipyakurel@Keeyoshi.local>
2023-08-19 12:56:37 +0545

    46

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
+const { body, validationResult } = require("express-validator");
 
 // create user using: POST "/api/auth". Doesn't required auth
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send("Hello");
-});
+router.post(
+  "/",
+  [
+    body("email", "Enter A Valid Email").isEmail(),
+    body("name", "Enter A Valid Name").isLength({ min: 3 }),
+    body("password", "Password Must Be More Than 4").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "Please Enter A Unique Value For Email",
+          message: err.message,
+        });
+      });
+  }
+);
 
 module.exports = router;
