97212f9047de99ec640679be34cfe8c3b2dff6e5
Keeyoshi Pyakurel <keeyoshipyakurel@Keeyoshi.local>
2023-08-19 16:12:20 +0545

    Login and Register Backend 50

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -48,9 +48,46 @@ router.post(
       res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error Occur");
+      res.status(500).send("Internal Server Error");
     }
   }
 );
 
+// Authenticate user using: POST "/api/auth/login". Doesn't required auth
+router.post(
+  "/login",
+  [
+    body("email", "Enter A Valid Email").isLength({ min: 3 }),
+    body("password", "Password cannot be blanked").exists(),
+  ],
+  async (req, res) => {
+    //IF errors are found, returns bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Email or Password Incorrect" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Email or Password Incorrect" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 module.exports = router;
