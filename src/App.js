e4240f856b1f6dfb30e329e4f2e9e05e52a72ac6
Keeyoshi Pyakurel <keeyoshipyakurel@Keeyoshi.local>
2023-08-20 18:15:59 +0545

    56 navbar and routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="container-fluid my-3">This is Home</div>
+    </>
+  );
+};
+
+export default Home;
